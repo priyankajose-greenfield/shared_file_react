167a8e1e51b9f4740727c0474bf6b29fe9815009
priyanka jose <priyanka@greenfieldmarine.net>
2025-09-14 06:38:37 +0400

    Initial commit: Next.js + MUI LAN shared form

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',   // enables static export
+  images: {
+    unoptimized: true // required for GitHub Pages
+  },
+  basePath: "/shared_file_react", // 👈 add repo name here
+  assetPrefix: "/shared_file_react/", // 👈 ensures assets load correctly
 };
+
 module.exports = nextConfig;
